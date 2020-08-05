3165458a7bc46adc497b1c6f3d040ebbcf76e2ab
Karl Hughes <khughes.me@gmail.com>
2020-08-05 05:54:37 -0500

    Fixing depth

--- frontend-deployer/script.sh
+++ frontend-deployer/script.sh
@@ -4,7 +4,7 @@ export NODE_ENV=development
 export GATSBY_AIRTABLE_API_KEY="$AT_API_KEY"
 git config --global user.email "autodeployer@pcto.co"
 git config --global user.name "autodeployer"
-git clone –-depth 1 https://karllhughes:$GITHUB_API_KEY@github.com/cfpland/frontend
+git clone --depth 1 https://karllhughes:$GITHUB_API_KEY@github.com/cfpland/frontend
 cd frontend
 npm install
 npm run deploy
