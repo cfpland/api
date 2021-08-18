a0c60f12d0e6c7a31f7aa28ea16d6b2f8f047704
Karl Hughes <khughes.me@gmail.com>
2021-08-18 12:31:31 -0500

    Adding new ad

--- src/shared/modules/email-builder/templates/weekly.ts
+++ src/shared/modules/email-builder/templates/weekly.ts
@@ -58,22 +58,21 @@ export const weeklyMjmlTemplate = `<mjml>
       </mj-column>
     </mj-section>
     
-    <!-- Sponsor: Draft -->
+    <!-- Sponsor -->
     <mj-section background-color="#e0ebf9" border-bottom="1px dashed lightgrey">
-      <mj-column width="30%">
-        <mj-image align="center" href="https://draft.dev/write" src="https://drive.google.com/uc?id=1qlldMq884v-DGyQLVHMP7mM36ZEetXFX"></mj-image>
+      <mj-column width="25%">
+        <mj-image align="center" href="https://www.cloud-native-conf.wtf/cfp?utm_source=cfpland&utm_medium=text&utm_campaign=CNconference" src="https://drive.google.com/uc?id=13c2_tYMvuCxHbyHZFfYA5inDOWNGcjRu"></mj-image>
       </mj-column>
-      <mj-column width="70%">
-        <mj-text font-weight="bold" line-height="24px">
-          <a href="https://draft.dev/write">We Pay Software Developers to Write</a>
+      <mj-column width="75%">
+        <mj-text font-weight="bold">
+          <a href="https://www.cloud-native-conf.wtf/cfp?utm_source=cfpland&utm_medium=text&utm_campaign=CNconference">WTF is Cloud Native?</a>
         </mj-text>
-        <mj-text color="#666666" font-size="16px" line-height="20px">
-          Draft.dev works with diverse writers from around the world to create blog posts and tutorials written
-          for a technical audience. Raise your profile, work with professional editors, and make a little money
-          along the way!
+        <mj-text color="#666666" font-size="14px" line-height="20px">
+          We believe that a proper Cloud Native transformation cannot happen without the right strategies and cultural changes.
+          Submit your talk proposal today for a chance to join our hybrid conference this fall.
         </mj-text>
         <mj-text>
-          <a href="https://draft.dev/write">Apply today →</a>
+          <a href="https://www.cloud-native-conf.wtf/cfp?utm_source=cfpland&utm_medium=text&utm_campaign=CNconference">Learn more and apply now! →</a>
         </mj-text>
         <mj-text align="right" font-size="12px" font-style="italic">Sponsor</mj-text>
       </mj-column>
